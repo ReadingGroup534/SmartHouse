ea673256637bce7055c28eaf801c24dbee6546b6
liyangchao <liyangchao@9.76.178.52>
2015-03-27 22:37:29 +0800

    update UI, changed the navigation bar from top to bottom, using RadioGroup and RadioButton

--- src/com/smarthouse/activity/MainActivity.java
+++ src/com/smarthouse/activity/MainActivity.java
@@ -12,51 +12,38 @@ import com.smarthouse.handler.OthersHandler;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Display;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.View.MeasureSpec;
 import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SearchView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 @SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
-	private TextView doorTextView,lightsTextView,othersTextView;
+	private RadioButton doorButton,lightsButton,othersButton,settingButton;
+	private RadioGroup radioGroup;
+//	doorTextView,lightsTextView,othersTextView;
 	private static MainActivity instanceActivity = null;
 	private ViewPager mViewPager;
 	private int currIndex = 0;
 	private int ScreenWidth;
 	private int one,two;
-	private LinearLayout friend_circle,scan,shake,nearby,bottle;
 	//listview 实例对象
 //	private ListView mListView;
 	private TextView doorDetails,lightsDetails,othersDetails;
@@ -105,27 +92,53 @@ public class MainActivity extends Activity {
 		findViewById();
 		
 		mViewPager.setOnPageChangeListener(new MyOnPageChangeListener());
-		doorTextView.setOnClickListener(new MyOnClickListener(0));
-		lightsTextView.setOnClickListener(new MyOnClickListener(1));
-		othersTextView.setOnClickListener(new MyOnClickListener(2));
-		
 				
 		DisplayMetrics dm = getResources().getDisplayMetrics();
 		ScreenWidth = dm.widthPixels;
 		one = ScreenWidth/3;
 		two = one*2;
 		
+		
 		//page in V4
 		LayoutInflater layoutInflater = LayoutInflater.from(this);
 		View view1 = layoutInflater.inflate(R.layout.door_details, null);
 		View view2 = layoutInflater.inflate(R.layout.lights_details, null);
 		View view3 = layoutInflater.inflate(R.layout.door_details, null);
+		View view4 = layoutInflater.inflate(R.layout.setting_layout, null);	
 		
-	  //获取每个页面对应按钮的实例
-//		mListView = (ListView) view1.findViewById(R.id.listview);
+	    //获取每个页面对应按钮的实例
 	    doorDetails = (TextView) view1.findViewById(R.id.doorDetails);
 	    lightsDetails = (TextView) view2.findViewById(R.id.lightsDetails);
 		
+	    radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup arg0, int checkedId) {
+				// TODO Auto-generated method stub
+				switch (checkedId) {
+				case R.id.door:
+					setTabSelection(0);
+					break;
+				case R.id.lights:
+					// 当点击了灯光tab时，选中第2个tab
+					setTabSelection(1);
+					break;
+				case R.id.others:
+					// 当点击了其他tab时，选中第3个tab
+					setTabSelection(2);
+					break;
+				case R.id.setting:
+					// 当点击了设置tab时，选中第4个tab
+					setTabSelection(3);
+					break;
+				default:
+					break;
+				}
+			}
+		});
+	    //默认选中第一个
+	    setTabSelection(0);
+	    
 		/*shake.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -138,11 +151,11 @@ public class MainActivity extends Activity {
 			}
 		});*/
 		
-		
-		final ArrayList<View> views = new ArrayList<View>();
+	    final ArrayList<View> views = new ArrayList<View>();
 		views.add(view1);
 		views.add(view2);
 		views.add(view3);
+		views.add(view4);
 		
 		doorHanlder = new DoorHanlder(this);
 		lightsHandler = new LightsHandler(this);
@@ -172,8 +185,36 @@ public class MainActivity extends Activity {
 			}
 		};
 		mViewPager.setAdapter(mPagerAdapter);
+		
 	}
 
+
+	/**
+	 * 根据传入的index参数来设置选中的tab页。
+	 * @param i
+	 * 每个tab页对应的下标。0表示房门，1表示灯光，2表示其他，3表示设置。
+	 */
+	protected void setTabSelection(int index) {
+		// TODO Auto-generated method stub
+		// 每次选中之前先清除掉上次的选中状态
+		clearSelection();
+		switch (index) {
+		case 0:
+			setButton(doorButton);
+			break;
+		case 1:
+			setButton(lightsButton);
+			break;
+		case 2:
+			setButton(othersButton);
+			break;
+		case 3:
+			setButton(settingButton);
+		default:
+			break;
+		}
+		mViewPager.setCurrentItem(index);
+	}
 
 	public void UpdateDoorDetails(String string) {
 		doorDetails.setText(string);
@@ -188,27 +229,13 @@ public class MainActivity extends Activity {
 	}
 	
 	private void findViewById(){
-		doorTextView = (TextView) findViewById(R.id.door);
-		lightsTextView = (TextView) findViewById(R.id.lights);
-		othersTextView = (TextView) findViewById(R.id.others);
+		radioGroup = (RadioGroup) findViewById(R.id.foot_radio_group);
+		doorButton = (RadioButton) findViewById(R.id.door);
+		lightsButton = (RadioButton) findViewById(R.id.lights);
+		othersButton = (RadioButton) findViewById(R.id.others);
+		settingButton = (RadioButton) findViewById(R.id.setting);
 		mViewPager = (ViewPager) findViewById(R.id.tabpager);
 	}
-	
-	
-	
-	
-	public class MyOnClickListener implements View.OnClickListener {
-		private int index = 0;
-
-		public MyOnClickListener(int i) {
-			index = i;
-		}
-
-		@Override
-		public void onClick(View v) {
-			mViewPager.setCurrentItem(index);
-		}
-	};
 	
 	
 	//page change method
@@ -229,48 +256,30 @@ public class MainActivity extends Activity {
 		@Override
 		public void onPageSelected(int arg0) {
 			// TODO Auto-generated method stub
-			Animation animation = null;
-			
+			clearSelection();
 			switch (arg0) {
 			case 0:
-				doorTextView.setTextColor(Color.GREEN);
-				if(currIndex == 1){
-//					animation = new TranslateAnimation(one, 0,0,0);
-					lightsTextView.setTextColor(Color.BLACK);
-				}else if (currIndex == 2) {
-//					animation = new TranslateAnimation(two, 0,0,0);
-					othersTextView.setTextColor(Color.BLACK);
-				}
+				setButton(doorButton);
 				break;
 			case 1:
-				lightsTextView.setTextColor(Color.GREEN);
-				if(currIndex == 0){
-//					animation = new TranslateAnimation(zero,one,0,0);
-					doorTextView.setTextColor(Color.BLACK);
-				}else if (currIndex == 2) {
-//					animation = new TranslateAnimation(two,one,0,0);
-					othersTextView.setTextColor(Color.BLACK);
-				}
+				setButton(lightsButton);
 				break;
 			case 2:
-				othersTextView.setTextColor(Color.GREEN);
-				if(currIndex == 0){
-//					animation = new TranslateAnimation(zero,two,0,0);
-					doorTextView.setTextColor(Color.BLACK);
-				}else if (currIndex == 1) {
-//					animation = new TranslateAnimation(one, two,0,0);
-					lightsTextView.setTextColor(Color.BLACK);
-				}
+				setButton(othersButton);
 				break;
+			case 3:
+				setButton(settingButton);
 			default:
 				break;
 			}
-			currIndex = arg0;
-//			animation.setFillAfter(true);
-//			animation.setDuration(150);
 		}
-		
 	}
+	
+	private void setButton(RadioButton button){
+		button.setChecked(true);
+		button.setTextColor(Color.GREEN);
+	}
+	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -285,6 +294,17 @@ public class MainActivity extends Activity {
 	}
 	
 
+
+	private void clearSelection() {
+		doorButton.setChecked(false);
+		doorButton.setTextColor(Color.WHITE);
+		lightsButton.setChecked(false);
+		lightsButton.setTextColor(Color.WHITE);
+		othersButton.setChecked(false);
+		othersButton.setTextColor(Color.WHITE);
+		settingButton.setChecked(false);
+		settingButton.setTextColor(Color.WHITE);
+	}
 
 	@Override
 	/*public boolean onOptionsItemSelected(MenuItem item) {
